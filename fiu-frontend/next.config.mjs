0534661ebfa03e721860fb8cf4c04140432eabb2
Nikesh Adhikari <adhikariraj016@gmail.com>
2026-02-13 01:50:23 +0545

    still on logo

--- fiu-frontend/next.config.mjs
+++ fiu-frontend/next.config.mjs
@@ -3,22 +3,7 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3000',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'localhost',
-        port: '3000',
-        pathname: '/**',
-      },
-    ],
-  },
+  
   
 };
 
--- fiu-frontend/src/components/grammr.js
+++ fiu-frontend/src/components/grammr.js
@@ -3,14 +3,14 @@ import Image from "next/image";
 export default function Logo() {
   return (
     <div className="w-40 xs:w-52 sm:w-60 md:w-72 lg:w-80 xl:w-[350px] max-w-full h-auto flex items-center">
-      <Image
+      <img
         src="/images/grammr.png"
         alt="FIU Logo"
         width={350}
         height={250}
         className="w-full h-auto "
         sizes="(max-width: 480px) 160px, (max-width: 640px) 208px, (max-width: 768px) 240px, (max-width: 1024px) 288px, (max-width: 1280px) 320px, 350px"
-        priority
+        
       />
     </div>
   );
